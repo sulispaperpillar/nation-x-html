ee30ea435ebadf399a5400949b81014a7339b455
Sulis Triyono <sulis@paperpillar.com>
2021-01-29 11:42:45 +0700

    Initial commit.

---
+++ assets/js/scripts.js
@@ -0,0 +1,77 @@
+(function($) {
+  "use strict";
+
+  // ------------------------------
+  // Menu
+  // ------------------------------
+  $('.js-mobile-menu').on('click', function() {
+    $(this).closest('header').toggleClass('open');
+    $('body').toggleClass('fixed');
+  });
+
+  // ------------------------------
+  // Carousel
+  // ------------------------------
+  $('.about-testimonials-carousel').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: $('.btn-arrow-prev'),
+    nextArrow: $('.btn-arrow-next'),
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.about-brand-carousel').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.case-study-carousel').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.btn-arrow-prev'),
+    nextArrow: $('.btn-arrow-next'),
+    dots: true
+  });
+
+  // ------------------------------
+  // Tab
+  // ------------------------------
+  $('.btn-tab').on('click', function() {
+    var dataTab = $(this).data('tab');
+    $(this).addClass('active').siblings().removeClass('active');
+    $('.tab-panel[data-target="' + dataTab + '"]').addClass('active').siblings().removeClass('active');
+  });
+
+})(jQuery);
